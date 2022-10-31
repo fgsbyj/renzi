8ec15ba028a3fcfafd95c5f19e53df4f5111b14e
FGS <1228908109.@qq.com>
2022-10-31 15:52:56 +0800

    第二次提交

--- vue.config.js
+++ vue.config.js
@@ -36,10 +36,9 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
   },
-  configureWebpack: {
-    // provide the app's title in webpack's name field, so that
+  configureWwebpackebpack: {
+    // provide the app's title in 's name field, so that
     // it can be accessed in index.html to inject the correct title.
     name: name,
     resolve: {
